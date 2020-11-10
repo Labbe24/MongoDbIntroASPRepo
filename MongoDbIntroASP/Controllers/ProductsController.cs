55c0cf604c184b8b6be3b81608726c8233788d82
Lasse Andresen <lasseandresen.1996@hotmail.com>
2020-11-10 15:00:13 +0100

    Add project files.

---
+++ MongoDbIntroASP/Controllers/ProductsController.cs
@@ -0,0 +1,74 @@
+﻿using MongoDbIntroASP.Models;
+using MongoDbIntroASP.Services;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace MongoDbIntroASP.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly ProductService _bookService;
+
+        public ProductsController(ProductService bookService)
+        {
+            _bookService = bookService;
+        }
+
+        [HttpGet]
+        public ActionResult<List<ProductCatalog>> Get() =>
+            _bookService.Get();
+
+        [HttpGet("{id:length(24)}", Name = "GetBook")]
+        public ActionResult<ProductCatalog> Get(string id)
+        {
+            var book = _bookService.Get(id);
+
+            if (book == null)
+            {
+                return NotFound();
+            }
+
+            return book;
+        }
+
+        [HttpPost]
+        public ActionResult<ProductCatalog> Create(ProductCatalog book)
+        {
+            _bookService.Create(book);
+
+            return CreatedAtRoute("GetBook", new { id = book.Id.ToString() }, book);
+        }
+
+        [HttpPut("{id:length(24)}")]
+        public IActionResult Update(string id, ProductCatalog bookIn)
+        {
+            var book = _bookService.Get(id);
+
+            if (book == null)
+            {
+                return NotFound();
+            }
+
+            _bookService.Update(id, bookIn);
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id:length(24)}")]
+        public IActionResult Delete(string id)
+        {
+            var book = _bookService.Get(id);
+
+            if (book == null)
+            {
+                return NotFound();
+            }
+
+            _bookService.Remove(book.Id);
+
+            return NoContent();
+        }
+    }
+}
---
+++ MongoDbIntroASP/Services/ProductService.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MongoDB.Driver;
+using MongoDbIntroASP.Models;
+
+namespace MongoDbIntroASP.Services
+{
+    public class ProductService
+    {
+        private readonly IMongoCollection<ProductCatalog> _products;
+
+        public ProductService(IProductCatalogDatabaseSettings settings)
+        {
+            var client = new MongoClient(settings.ConnectionString);
+            var database = client.GetDatabase(settings.DatabaseName);
+
+            _products = database.GetCollection<ProductCatalog>(settings.ProductsCollectionName);
+        }
+
+        public List<ProductCatalog> Get() =>
+            _products.Find(book => true).ToList();
+
+        public ProductCatalog Get(string id) =>
+            _products.Find<ProductCatalog>(product => product.Id == id).FirstOrDefault();
+
+        public ProductCatalog Create(ProductCatalog product)
+        {
+            _products.InsertOne(product);
+            return product;
+        }
+
+        public void Update(string id, ProductCatalog productIn) =>
+            _products.ReplaceOne(product => product.Id == id, productIn);
+
+        public void Remove(ProductCatalog productIn) =>
+            _products.DeleteOne(product => product.Id == productIn.Id);
+
+        public void Remove(string id) =>
+            _products.DeleteOne(product => product.Id == id);
+    }
+}
